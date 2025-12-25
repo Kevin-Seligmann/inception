85f092d9131ef366b56b855c971edf8f52cf11f9
Kseligma <seligmann.kevin.cc@gmail.com>
2025-12-25 21:17:05 +0100

    Inc

--- project/srcs/requirements/mariadb/tools/mariadb_healthcheck.sh
+++ project/srcs/requirements/mariadb/tools/mariadb_healthcheck.sh
@@ -1,3 +1,4 @@
 # Check mariadb is running and wordpress credentials exist
 
 mariadb -u ${WP_DATABASE_ADMIN_USERNAME} -p${WP_DATABASE_ADMIN_PASSWORD} -e "USE ${WP_DATABASE_NAME}" || exit 1
+
--- project/srcs/requirements/mariadb/tools/mariadb_img_entry.sh
+++ project/srcs/requirements/mariadb/tools/mariadb_img_entry.sh
@@ -1,9 +1,9 @@
-#!bin/sh
+#!/bin/sh
 
 args="-u root -p${MARIADB_ROOT_PASSWORD} -e"
 
 # Install (Would do nothing if installed)
-mariadb-install-db --datadir="/var/lib/mysql" --auth-root-authentication-method=normal --skip-test-db
+mariadb-install-db --datadir="/var/lib/mysql" --auth-root-authentication-method=normal --skip-test-db --verbose	
 
 # Mariadb auxiliary process to execute initalization script
 mariadbd & MARIADB_PID=$!
@@ -14,9 +14,9 @@ for i in {25..0}; do
 		echo 'mariadb init started'
 		break
 	fi
-	if mariadb -u root "SELECT '1';" &> /dev/null; then
+	if mariadb -u root -e "SELECT '1';" &> /dev/null; then
 		echo 'mariadb init started'
-		break;
+		break
 	fi
 	sleep 1
 done
@@ -26,18 +26,33 @@ if [ "$i" = 0 ]; then
 	exit 1
 fi
 
+mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MARIADB_ROOT_PASSWORD}'); FLUSH PRIVILEGES;"
+echo "0. Setting root password " $? 
+
 # Changing root password after installation (Would do nothing if installed)
-mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MARIADB_ROOT_PASSWORD}'); FLUSH PRIVILEGES;"
-mariadb $args "SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('${MARIADB_ROOT_PASSWORD}');"
-mariadb $args "SET PASSWORD FOR 'root'@'::1' = PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+mariadb $args "SET PASSWORD FOR 'root'@'127.0.0.1' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+echo "1. Setting root password " $? 
+
+mariadb $args "SET PASSWORD FOR 'root'@'::1' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+echo "2. Setting root password " $? 
+
 # An user like 'root'@hostname is created
-mariadb $args "SET PASSWORD FOR 'root'@'${HOSTNAME}' = PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+mariadb $args "SET PASSWORD FOR 'root'@'${HOSTNAME}' IDENTIFIED VIA mysql_native_password USING PASSWORD('${MARIADB_ROOT_PASSWORD}');"
+echo "3. Setting root password " $? " " ${HOSTNAME} 
+
 mariadb $args "FLUSH PRIVILEGES;"
 
 # Creating wordpress database and user (Would do nothing if created)
 mariadb $args "CREATE DATABASE IF NOT EXISTS ${WP_DATABASE_NAME};"
-mariadb $args "CREATE USER IF NOT EXISTS '${WP_DATABASE_ADMIN_USERNAME}'@'%' IDENTIFIED BY '${WP_DATABASE_ADMIN_PASSWORD}';"
-mariadb $args "GRANT ALL ON ${WP_DATABASE_NAME}.* TO '${WP_DATABASE_ADMIN_USERNAME}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+echo "Creating database if not exists " ${WP_DATABASE_NAME} ": " $? 
+
+mariadb $args "CREATE USER IF NOT EXISTS '${WP_DATABASE_ADMIN_USERNAME}'@'%' IDENTIFIED VIA mysql_native_password USING PASSWORD('${WP_DATABASE_ADMIN_PASSWORD}');"
+echo "Creating WP User " ${WP_DATABASE_ADMIN_USERNAME} ": " $? 
+
+mariadb $args "GRANT ALL ON ${WP_DATABASE_NAME}.* TO '${WP_DATABASE_ADMIN_USERNAME}'@'%' WITH GRANT OPTION;"
+echo "Granting WP User " ${WP_DATABASE_ADMIN_USERNAME} ": " $? 
+
+mariadb $args "FLUSH PRIVILEGES;"
 
 # Kill auxiliary process
 kill -s TERM "$MARIADB_PID"
@@ -45,4 +60,6 @@ kill -s TERM "$MARIADB_PID"
 wait "$MARIADB_PID"
 
 # Execute mariadb
-exec mariadbd-safe
+cd '/usr' 
+
+exec /usr/bin/mariadbd-safe --datadir='/var/lib/mysql'
